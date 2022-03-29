1ce223a6d73e46ab57faef7b0d503a8ff136b880
CT423927 <73744353+CT423927@users.noreply.github.com>
2022-03-29 17:50:46 +0200

    nuevo despliegue

--- deteccion-dolor/server.js
+++ deteccion-dolor/server.js
@@ -1,16 +1,14 @@
-//Install express server
-const express = require('express');
 const path = require('path');
-
+const express = require('express');
 const app = express();
 
-// Serve only the static files form the dist directory
-app.use(express.static(__dirname + '/dist/<name-of-app>'));
+// Serve static files
+app.use(express.static(__dirname + '/dist/deteccion-dolor'));
 
-app.get('/*', function(req,res) {
-    
-res.sendFile(path.join(__dirname+'/dist/<name-of-app>/index.html'));
+// Send all requests to index.html
+app.get('/*', function(req, res) {
+  res.sendFile(path.join(__dirname + '/dist/deteccion-dolor/index.html'));
 });
 
-// Start the app by listening on the default Heroku port
-app.listen(process.env.PORT || 8080);
+// default Heroku port
+app.listen(process.env.PORT || 5000);
