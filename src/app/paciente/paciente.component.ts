d869dcfa555f07d7b5bf73e1b4e079d947a5db1b
CT423927 <73744353+CT423927@users.noreply.github.com>
2022-05-07 19:31:34 +0200

    Cambios diseño página paciente

--- src/app/paciente/paciente.component.ts
+++ src/app/paciente/paciente.component.ts
@@ -1,18 +1,38 @@
-import { Component, OnInit } from '@angular/core';
+import { DOCUMENT } from '@angular/common';
+import { Component, Inject, OnInit, Renderer2 } from '@angular/core';
 import { ActivatedRoute, Params } from '@angular/router';
+import { OwlOptions } from 'ngx-owl-carousel-o';
+import { Injectable } from '@angular/core';
+import { map } from 'rxjs/operators';
+
+
+declare const scan: any;
 
 @Component({
   selector: 'app-paciente',
   templateUrl: './paciente.component.html',
   styleUrls: ['./paciente.component.scss']
 })
+
+@Injectable({
+  providedIn: 'root'
+})
+
 export class PacienteComponent implements OnInit {
+
+  listExpressions: any = [];
   id!: number;
   private sub: any;
 
+  onClick(){
+    scan();
+  }
+  
   constructor(private route: ActivatedRoute) {}
 
+
   ngOnInit() {
+
     this.sub = this.route.params.subscribe(params => {
        this.id = +params['id']; // (+) converts string 'id' to a number
 
@@ -20,8 +40,18 @@ export class PacienteComponent implements OnInit {
     });
   }
 
-  ngOnDestroy() {
-    this.sub.unsubscribe();
+
+  customOptions: OwlOptions = {
+    stagePadding: 0,
+    loop: false,
+    mouseDrag: true,
+    touchDrag: true,
+    pullDrag: true,
+    dots: false,
+    margin: 0,
+    navSpeed: 700,
+    nav: false,
+    items:4
   }
 
 }
